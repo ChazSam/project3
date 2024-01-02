cf1a0789101bd4c20817da7c79688bf2cb44dad9
Charles <Charlessammarco@gmail.com>
2024-01-02 15:19:21 -0600

    imports

---
+++ helpers.py
@@ -0,0 +1,2 @@
+from models.member import Member
+from models.trainer import Trainer
---
+++ seed.py
@@ -0,0 +1,4 @@
+from models.__init__ import CONN, CURSOR
+from models.member import Member
+from models.trainer import Trainer
+
