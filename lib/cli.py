0d03f3ce3770624f68c7c48feff068860f873087
Charles <Charlessammarco@gmail.com>
2024-01-03 13:12:35 -0600

    init and cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,1 +1,6 @@
-print("hello world")
+print("Welcome to Trainer App. Please enter a number for:\n" 
+      "1. List all trainers \n"
+      "2. Add a trainer\n"
+      "3. Change a trainers work days\n"
+      "4. Remove a trainer\n"
+      "5. Exit Program")
--- lib/models/trainer.py
+++ lib/models/trainer.py
@@ -2,5 +2,6 @@
 
 class Trainer:
     
-    def __init__(self, name):
+    def __init__(self, name, work_days):
         self.name = name
+        self.work_days = work_days
