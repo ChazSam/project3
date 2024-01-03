6f810eac30f29b0bfadc4974e6a0106365a83cdc
Charles <Charlessammarco@gmail.com>
2024-01-03 13:48:28 -0600

    added setters

--- lib/models/member.py
+++ lib/models/member.py
@@ -7,6 +7,39 @@ class Member:
         self.age = age
         self.goals = goals
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError ("Please enter a valid name")
+        
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        if isinstance(age, int) and age >= 18:
+            self._age = age
+        else:
+            raise ValueError ("Please enter a number - Members must be at least 18 years old")
+    
+    @property
+    def goals(self):
+        return self._goals
+    
+    @goals.setter
+    def goals(self,goals):
+        if isinstance(goals, str) and len(goals):
+            self._goals = goals
+        else:
+            raise ValueError ("Please enter a goal")
+
     def get_all(self):
         pass
 
--- lib/models/trainer.py
+++ lib/models/trainer.py
@@ -5,6 +5,28 @@ class Trainer:
     def __init__(self, name, work_days):
         self.name = name
         self.work_days = work_days
+   
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError ("Please enter a valid name")
+        
+    @property
+    def work_days(self):
+        return self._work_days
+    
+    @work_days.setter
+    def work_days(self, work_days):
+        if isinstance(work_days, str):
+            self._work_days = work_days
+        else:
+            raise ValueError ("Please enter days with a single letter and commas (M,T,W,H,F,S)")
 
     def get_all(self):
         pass
